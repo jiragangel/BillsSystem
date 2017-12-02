2d78a70b8f5f9dc5a82c08984475b2aa8af4321a
aloofrvr <kdbaylon@up.edu.ph>
2017-12-02 19:25:33 +0800

    billinformationsystem.sql

--- Bill Information System/billinformationsystem.sql
+++ Bill Information System/billinformationsystem.sql
@@ -8,11 +8,6 @@ create table SENATOR (
   CONSTRAINT SENATOR_Employeenumber_pk PRIMARY KEY(Employeenumber)
 );
 
-create table SENATOR_POSITION(
-  Employeenumber VARCHAR(10),
-  Position VARCHAR(20),
-  CONSTRAINT SENATOR_POSITION_enamepos_pk PRIMARY KEY(Employeenumber,Position)
-);
 
 create table SENATOR_COMMITTEE(
   Employeenumber VARCHAR(10),
@@ -22,7 +17,7 @@ create table SENATOR_COMMITTEE(
 
 create table BILL_SUBJECT(
   Billno varchar(10),
-  Subject varchar(20),
+  Subject varchar(50),
   CONSTRAINT BILL_SUBJECT_billnosubj_pk PRIMARY KEY(Billno,Subject)
 );
 
@@ -76,7 +71,6 @@ create table BILL(
   Status varchar(50),
   Title varchar(100),
   Summarydesc varchar(250),
-  Content varchar(1000),
   Primarycommittee varchar(50),
   Scope varchar(10),
   Secondarycommittee varchar(50),
@@ -89,41 +83,6 @@ create table BILL(
   CONSTRAINT BILL_Hemployeenumber_fk FOREIGN KEY(Hemployeenumber) REFERENCES HOUSEMEMBER(Employeenumber)
 );
 
--- INSERT VALUES
-insert into SENATOR values (
-  "5526601",
-  "Sonny Angara"
-);
-
-insert into BILL values (
-  "SB 1598",
-  "Pending in the Committee",
-  "Batas Kasambahay Social Security Condonation Law of 2017",
-  "An act granting the Social Security System the authority to coondone penalties for unremitted or delinquent contributes by employers or domestic workers for the purpose of encouraging compliance with Social Security Laws.",
-  "Too long",
-  "Labor, Employment and Human Resources Development",
-  "National",
-  NULL,
-  1,
-  "5526601",
-  NULL,
-  NULL
-);
-
-insert into BILL values (
-  "SB 1599",
-  "Pending in the Committee",
-  "Batas Kasambahay Social Security Condonation Law of 2017",
-  "An act granting the Social Security System the authority to coondone penalties for unremitted or delinquent contributes by employers or domestic workers for the purpose of encouraging compliance with Social Security Laws.",
-  "Too long",
-  "Labor, Employment and Human Resources Development",
-  "National",
-  NULL,
-  1,
-  "5526601",
-  NULL,
-  NULL
-);
 
 delimiter //
 CREATE PROCEDURE Filebillhousemem (
@@ -133,19 +92,16 @@ year int(4),
 status varchar(50),
 title varchar(100),
 summarydesc varchar(250),
-content varchar(1000),
 primaryC varchar(50),
 scope varchar(10),
 secondaryC varchar(50)
 )
 BEGIN
 insert into HOUSEMEMBER_FILES values (empno, billno, year);
-insert into BILL values (billno, status, title, summarydesc, content, primaryC, scope, secondaryC, NULL, NULL, 1, empno);
+insert into BILL values (billno, status, title, summarydesc,primaryC, scope, secondaryC, NULL, NULL, 1, empno);
 END
 //
 delimiter ;
-
-insert into HOUSEMEMBER values ("1101","Sonny Angara","What type?");
 
 delimiter //
 CREATE PROCEDURE Filebillsenator (
@@ -155,7 +111,6 @@ year int(4),
 status varchar(50),
 title varchar(100),
 summarydesc varchar(250),
-content varchar(1000),
 primaryC varchar(50),
 scope varchar(10),
 secondaryC varchar(50)
@@ -180,3 +135,408 @@ CREATE PROCEDURE DeleteBill ( billnumber varchar(10) )
   END
 //
 delimiter ;
+
+
+-- INSERT VALUES
+insert into SENATOR values (
+  "0001",
+  "Aquilino Pimentel III"
+);
+insert into SENATOR_COMMITTEE values(
+  "0001",
+  "Electoral Reforms and People Participation"
+);
+insert into SENATOR_COMMITTEE values(
+  "0001",
+  "Justice and Human Rights"
+);
+insert into SENATOR_COMMITTEE values(
+  "0002",
+  "Commission on Appointments"
+);
+
+insert into SENATOR values (
+  "0002",
+  "Maria Lourdes Nancy Binay"
+);
+insert into SENATOR_COMMITTEE values(
+  "0002",
+  "Social Justice, Welfare and Rural Development"
+);
+insert into SENATOR_COMMITTEE values(
+  "0002",
+  "Accounts"
+);
+insert into SENATOR_COMMITTEE values(
+  "0002",
+  "Banks, Financial Institutions and Currencies"
+);
+insert into SENATOR_COMMITTEE values(
+  "0002",
+  "Climate Change"
+);
+insert into SENATOR_COMMITTEE values(
+  "0002",
+  "Cultural Communities"
+);
+insert into SENATOR_COMMITTEE values(
+  "0002",
+  "Economic Affairs"
+);
+
+
+insert into SENATOR values (
+  "0003",
+  "Joel Villanueva"
+);
+insert into SENATOR_COMMITTEE values(
+  "0003",
+  "Accounts"
+);
+insert into SENATOR_COMMITTEE values(
+  "0003",
+  "Agrarian Reform"
+);
+insert into SENATOR_COMMITTEE values(
+  "0003",
+  "Banks, Financial Institutions and Currencies"
+);
+insert into SENATOR_COMMITTEE values(
+  "0003",
+  "Cooperatives"
+);
+insert into SENATOR_COMMITTEE values(
+  "0003",
+  "Cultural Communities"
+);
+
+insert into SENATOR values (
+  "0004",
+  "Leila De Lima"
+);
+insert into SENATOR_COMMITTEE values(
+  "0004",
+  "Electoral Reforms and People's Participation"
+);
+insert into SENATOR_COMMITTEE values(
+  "0004",
+  "Agrarian Reform"
+);
+insert into SENATOR_COMMITTEE values(
+  "0004",
+  "Energy"
+);
+insert into SENATOR_COMMITTEE values(
+  "0004",
+  "Health and Demography"
+);
+insert into SENATOR_COMMITTEE values(
+  "0004",
+  "National Defense and Security"
+);
+
+insert into SENATOR values (
+  "0005",
+  "Sherwin Gatchalian"
+);
+insert into SENATOR_COMMITTEE values(
+  "0005",
+  "Accountability of Public Officers and Investigations"
+);
+insert into SENATOR_COMMITTEE values(
+  "0005",
+  "Banks, Financial Institutions and Currencies"
+);
+insert into SENATOR_COMMITTEE values(
+  "0005",
+  "Climate Change"
+);
+insert into SENATOR_COMMITTEE values(
+  "0005",
+  "Economic Affairs"
+);
+insert into SENATOR_COMMITTEE values(
+  "0005",
+  "Education, Arts and Culture"
+);
+
+insert into SENATOR values (
+  "0006",
+  "Joseph Victor G. Ejercito"
+);
+insert into SENATOR_COMMITTEE values(
+  "0006",
+  "Urban Planning, Housing and Resettlement"
+);
+insert into SENATOR_COMMITTEE values(
+  "0006",
+  "Accounts"
+);
+insert into SENATOR_COMMITTEE values(
+  "0006",
+  "Agrarian Reform"
+);
+insert into SENATOR_COMMITTEE values(
+  "0006",
+  "Energy"
+);
+insert into SENATOR_COMMITTEE values(
+  "0006",
+  "Economic Affairs"
+);
+insert into SENATOR values (
+  "0007",
+  "Ralph G. Recto"
+);
+insert into SENATOR_COMMITTEE values(
+  "0007",
+  "Agriculture and Food"
+);
+insert into SENATOR_COMMITTEE values(
+  "0007",
+  "Accounts"
+);
+insert into SENATOR_COMMITTEE values(
+  "0007",
+  "Agrarian Reform"
+);
+insert into SENATOR_COMMITTEE values(
+  "0007",
+  "Banks, Financial Institutions and Currencies"
+);
+insert into SENATOR_COMMITTEE values(
+  "0007",
+  "Climate Change"
+);
+insert into SENATOR values (
+  "0008",
+  "Vicente C. Sotto III"
+);
+insert into SENATOR_COMMITTEE values(
+  "0008",
+  "Ethics and Privileges"
+);
+insert into SENATOR_COMMITTEE values(
+  "0008",
+  "Rules"
+);
+insert into SENATOR_COMMITTEE values(
+  "0008",
+  "Agrarian Reform"
+);
+insert into SENATOR_COMMITTEE values(
+  "0008",
+  "Banks, Financial Institutions and Currencies"
+);
+insert into SENATOR_COMMITTEE values(
+  "0008",
+  "Climate Change"
+);
+insert into SENATOR values (
+  "0009",
+  "Franklin M. Drilon"
+);
+insert into SENATOR_COMMITTEE values(
+  "0009",
+  "Accountability of Public Officers and Investigations"
+);
+insert into SENATOR_COMMITTEE values(
+  "0009",
+  "Accounts"
+);
+insert into SENATOR_COMMITTEE values(
+  "0009",
+  "Agrarian Reform"
+);
+insert into SENATOR_COMMITTEE values(
+  "0009",
+  "Agriculture and Food"
+);
+insert into SENATOR_COMMITTEE values(
+  "0009",
+  "Banks, Financial Institutions and Currencies"
+);
+insert into SENATOR values (
+  "0010",
+  "Sonny M. Angara"
+);
+insert into SENATOR_COMMITTEE values(
+  "0010",
+  "Games, Amusements and Sports"
+);
+insert into SENATOR_COMMITTEE values(
+  "0010",
+  "Labor Employment & Human Resources Development"
+);
+insert into SENATOR_COMMITTEE values(
+  "0010",
+  "Ways and Means"
+);
+insert into SENATOR_COMMITTEE values(
+  "0010",
+  "Agrarian Reform"
+);
+insert into SENATOR_COMMITTEE values(
+  "0010",
+  "Constitutional Amendments and Revision of Codes"
+);
+insert into SENATOR values (
+  "0011",
+  "Paolo Benigno Aquino"
+);
+insert into SENATOR_COMMITTEE values(
+  "0011",
+  "Trade, Commerce and Entrepreneurship"
+);
+insert into SENATOR_COMMITTEE values(
+  "0011",
+  "Youth"
+);
+insert into SENATOR_COMMITTEE values(
+  "0011",
+  "Agrarian Reform"
+);
+insert into SENATOR_COMMITTEE values(
+  "0011",
+  "Accountability of Public Officers and Investigations"
+);
+insert into SENATOR_COMMITTEE values(
+  "0011",
+  "Accounts"
+);
+
+insert into SENATOR values (
+  "0012",
+  "Alan Peter S. Cayetano"
+);
+insert into SENATOR_COMMITTEE values(
+  "0012",
+  "Rules"
+);
+insert into SENATOR_COMMITTEE values(
+  "0012",
+  "Justice and Human Rights"
+);
+insert into SENATOR_COMMITTEE values(
+  "0012",
+  "National Defense and Security"
+);
+insert into SENATOR_COMMITTEE values(
+  "0012",
+  "Women, Children, Family Relations and Gender Equality"
+);
+
+
+FileBillSenator (
+  "0002",
+  "SB 1598",
+  2017,
+  "Pending in the Committee",
+  "Batas Kasambahay Social Security Condonation Law of 2017",
+  "An act granting the Social Security System the authority to coondone penalties for unremitted or delinquent contributes by employers or domestic workers for the purpose of encouraging compliance with Social Security Laws.",
+  "Labor, Employment and Human Resources Development",
+  "National",
+  NULL,
+);
+insert into BILL_SUBJECT values(
+  "SB 1598",
+  "Social Security System (SSS)"
+);
+insert into BILL_SUBJECT values(
+  "SB 1598",
+  "Domestic Worker"
+);
+
+insert into BILL_SUBJECT values(
+  "SB 1598",
+  "Batas Kasambahay (R.A.No. 10361)"
+);
+
+insert into BILL_SUBJECT values(
+  "SB 1598",
+  "Condonation"
+);
+
+FileBillSenator (
+  "0003",
+  "SB 1614",
+  2017,
+  "Pending in the Committee",
+  "Service Incentive Leave Of Employees",
+  "An act increasing the service incentive leave of employees, amending for the purpose Article 95 of Presidential Decree No. 442, as amended, otherwise known as the Labor Code of the Philippines.",
+  "Labor, Employment and Human Resources Development",
+  "National",
+  NULL,
+);
+insert into BILL_SUBJECT values(
+  "SB 1614",
+  "Labor Code of the Philippines(P.D.No.442)"
+);
+insert into BILL_SUBJECT values(
+  "SB 1614",
+  "Leave of Absence"
+);
+
+FileBillSenator (
+  "0004",
+  "SB 1622",
+  2017,
+  "Pending in the Committee",
+  "Parking Space for Handicapped Persons Act of 2017",
+  "An act mandating the provision of exclusive parking space for handicapped persons and penalizing the unauthorized use thereof.",
+  "Social Justice, Welfare and Rural Development",
+  "National",
+  NULL,
+);
+insert into BILL_SUBJECT values(
+  "SB 1622",
+  "Handicapped/Disabled/Differently Abled"
+);
+insert into BILL_SUBJECT values(
+  "SB 1622",
+  "Persons with Disabilities (PWDS)"
+);
+insert into BILL_SUBJECT values(
+  "SB 1622",
+  "Parking Space"
+);
+FileBillSenator (
+  "0004",
+  "SB 1622",
+  2017,
+  "Pending in the Committee",
+  "Nutritional Information Disclosure Act of 2017",
+  "An act requiring the disclosure of nutritional information in menus of food service establishments.",
+  "Health and Demography",
+  "National",
+  NULL,
+);
+insert into BILL_SUBJECT values(
+  "SB 1622",
+  "Nutrition"
+);
+insert into BILL_SUBJECT values(
+  "SB 1622",
+  "Food Establishments"
+);
+
+FileBillSenator (
+  "0006",
+  "SB 1619",
+  2017,
+  "Pending in the Committee",
+  "Anti-Discrimination Act of 2017",
+  "An act prohibiting discrimination on the basis of ethnicity, race, religion or belief, sex, gender, sexual orientation, gender identity, gender expression, civil status and HIV status, and providing penalties therefor.",
+  "Cultural Communities",
+  "National",
+  "Social Justice, Welfare and Rural Development",
+);
+insert into BILL_SUBJECT values(
+  "SB 1619",
+  "Anti-Discrimination"
+);
+
+
+
+
+
